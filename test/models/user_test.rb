3677a76e695193f56c7f2e8c7aedfca11ff8650d
Jason Loutensock <jason.loutensock@acimacredit.com>
2020-09-19 20:10:58 +0000

    add test for practice

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -51,4 +51,11 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     assert_not duplicate_user.valid?
   end
+  
+  test "email adresses should be saved as lowercase" do
+    mixed_case_email = "Foo@ExamPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
 end
