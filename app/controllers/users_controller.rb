16b721ab96f471f00d161e9c12815ec77157e6c4
Jason Loutensock <jason.loutensock@acimacredit.com>
2020-10-11 18:40:52 +0000

    redirect to user page after successful signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # handle a successful save
+      redirect_to user_url(@user)
     else
       render 'new'
     end
