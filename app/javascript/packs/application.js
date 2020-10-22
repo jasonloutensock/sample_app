60c6232edf9c8458695f4499dd14e6be8928ca8b
Jason Loutensock <jason.loutensock@acimacredit.com>
2020-10-22 13:09:18 +0000

    fix bootstrap bug

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,13 +3,12 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/activestorage").start();
 require("jquery");
 require("@rails/ujs").start();
+require("@rails/activestorage").start();
+require("bootstrap");
 require("turbolinks").start();
 require("channels");
-import "bootstrap";
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
