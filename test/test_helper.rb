b0cf6cd4bc90ce4169c91970836ec52f20c4a5e7
Jason Loutensock <jason.loutensock@acimacredit.com>
2020-10-27 18:15:15 +0000

    add test helper is_logged_in?

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 require "minitest/reporters"
+
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
@@ -11,6 +12,10 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   include ApplicationHelper
+  
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 
   # Add more helper methods to be used by all tests here...
 end
